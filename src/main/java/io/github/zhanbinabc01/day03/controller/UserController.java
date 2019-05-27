2058624f281a8106ba3c84d238924f76543a0ff2
zhanbinabc01 <624289353@qq.com>
2019-05-27 17:37:52 +0800

    v1测试提交更新+5

--- src/main/java/io/github/zhanbinabc01/day03/controller/UserController.java
+++ src/main/java/io/github/zhanbinabc01/day03/controller/UserController.java
@@ -9,7 +9,7 @@ public class UserController {
 	
 	@RequestMapping("show_index")
 	public String showIndex(Model model) {
-		System.out.println("day03原始版本测试提交更新+4");
+		System.out.println("v1测试提交更新+5(未提交)");
 		model.addAttribute("msg", "userController传来信息");
 		model.addAttribute("msg3", "userController传来信息3");
 		model.addAttribute("msg2", "userController传来信息2");
