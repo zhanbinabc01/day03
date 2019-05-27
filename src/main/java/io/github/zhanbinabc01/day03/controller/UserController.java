cb17f6868fb40722690bebbf56ca534e8c7e45ac
zhanbinabc01 <624289353@qq.com>
2019-05-27 17:08:30 +0800

    day03 提交更新

--- src/main/java/io/github/zhanbinabc01/day03/controller/UserController.java
+++ src/main/java/io/github/zhanbinabc01/day03/controller/UserController.java
@@ -9,6 +9,7 @@ public class UserController {
 	
 	@RequestMapping("show_index")
 	public String showIndex(Model model) {
+		System.out.println("day03");
 		model.addAttribute("msg", "userController传来信息");
 		model.addAttribute("msg3", "userController传来信息3");
 		model.addAttribute("msg2", "userController传来信息2");
