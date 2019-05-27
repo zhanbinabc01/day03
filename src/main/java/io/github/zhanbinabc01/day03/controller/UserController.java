5f50a339db6ee7cd96ff52733806e8704a00cf49
zhanbinabc01 <624289353@qq.com>
2019-05-27 12:25:35 +0800

    控制层想页面传递Model信息

--- src/main/java/io/github/zhanbinabc01/day03/controller/UserController.java
+++ src/main/java/io/github/zhanbinabc01/day03/controller/UserController.java
@@ -1,13 +1,15 @@
 package io.github.zhanbinabc01.day03.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
 	
 	@RequestMapping("show_index")
-	public String showIndex() {
+	public String showIndex(Model model) {
+		model.addAttribute("msg", "userController传来信息");
 		return "index";
 	}
 }
